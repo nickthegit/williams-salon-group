981846f2ca61cbb755515dd46377f89d8d89bf9c
nickthegit <nick@rosielee.co.uk>
2018-10-29 15:01:06 +0000

    add nuxt

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+
+  </section>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+
+// export default {
+//   components: {
+//     Logo
+//   }
+// }
+</script>
+
+<style>
+</style>
